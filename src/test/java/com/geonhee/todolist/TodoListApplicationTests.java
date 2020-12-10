5fb684b91f33050ae90a5306f0f009f970b5ece8
geonhee.sung <jvckigh@gmail.com>
2020-12-10 20:28:56 +0900

    initial setting

---
+++ src/test/java/com/geonhee/todolist/TodoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geonhee.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
