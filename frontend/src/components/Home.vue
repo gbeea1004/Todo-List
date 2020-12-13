150f76c41037ffd0ffd87b5e8088f651f8ae751a
Sung Geon Hee <geonhee.sung@nhntoast.com>
2020-12-13 21:41:37 +0900

    Add navigation bar

---
+++ frontend/src/components/Home.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    HOME
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            tasks: [
+                {
+                    done: false,
+                    text: 'Foobar',
+                },
+                {
+                    done: false,
+                    text: 'Fizzbuzz',
+                },
+            ],
+            newTask: null
+        }
+    },
+    methods: {
+        create() {
+            this.tasks.push({
+                done: false,
+                text: this.newTask,
+            })
+
+            this.newTask = null
+        }
+    },
+    computed: {
+      completedTasks () {
+        return this.tasks.filter(task => task.done).length
+      },
+      progress () {
+        return this.completedTasks / this.tasks.length * 100
+      },
+      remainingTasks () {
+        return this.tasks.length - this.completedTasks
+      },
+    }
+}
+</script>
+
+<style>
+
+</style>
